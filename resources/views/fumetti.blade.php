465ab21f9f358455daa30f52414381686f673a0d
giorgios17 <giosur@live.it>
2022-05-18 17:20:12 +0200

    various

---
+++ resources/views/fumetti.blade.php
@@ -0,0 +1,9 @@
+<?php
+@include '../../config/comics.php';
+?>
+@foreach ($fumetti as $fumetto)
+    <div class="card">
+        <img src="{{ $fumetto['thumb'] }}" :alt="{{ $fumetto['series'] }}" />
+        <p>{{ $fumetto['series'] }}</p>
+    </div>
+@endforeach
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/fumetti', function () {
+    $fumetti = config('comics');
+    return view('fumetti', ["fumetti"=> $fumetti]);
+});
