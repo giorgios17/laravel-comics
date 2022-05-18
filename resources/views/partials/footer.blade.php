5872be5bb53bd02c9653c9be58b81fc5adff076e
giorgios17 <giosur@live.it>
2022-05-18 16:57:56 +0200

    main

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,61 @@
 <footer>
+    <!-- shop link -->
+    <section class="infolink">
+        <div class="container">
+            {{-- <div class="info" :key="index" v-for="(link, index) in infolink">
+                <img :src="link.image" :alt="link.name" />
+                <p>{{ link . name }}</p>
+            </div> --}}
+            <div class="info">
 
+            </div>
+        </div>
+    </section>
+    <!-- footer nav -->
+    <section class="footernav">
+        <div class="container">
+            <div class="col">
+                <ul>
+                    <h3>DC COMICS</h3>
+                    {{-- <li :key="index" v-for="(link, index) in dcComics">
+                        <a href="#">{{ link }}</a>
+                    </li> --}}
+                </ul>
+                <ul>
+                    <h3>SHOP</h3>
+                    {{-- <li :key="index" v-for="(link, index) in shop">
+                        <a href="#">{{ link }}</a>
+                    </li> --}}
+                </ul>
+            </div>
+            <div class="col">
+                <ul>
+                    <h3>DC</h3>
+                    {{-- <li :key="index" v-for="(link, index) in dc">
+                        <a href="#">{{ link }}</a>
+                    </li> --}}
+                </ul>
+            </div>
+            <div class="col">
+                <ul>
+                    <h3>SITES</h3>
+                    {{-- <li :key="index" v-for="(link, index) in sites">
+                        <a href="#">{{ link }}</a>
+                    </li> --}}
+                </ul>
+            </div>
+        </div>
+    </section>
+    <!-- social link & signup -->
+    <section class="footersocial">
+        <div class="container">
+            <a id="signup" href="#">SIGN-UP NOW!</a>
+            <div class="social">
+                <p>FOLLOW US</p>
+                {{-- <div :key="index" v-for="(social, index) in allSocial">
+                    <img :src="social" alt="social" />
+                </div> --}}
+            </div>
+        </div>
+    </section>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,16 @@
 <header>
+    {{-- NAVLINKS --}}
+    <?php
+    $navlinks = ['CHARACTERS', 'COMICS', 'MOVIES', 'TV', 'GAMES', 'COLLECTIBLES', 'VIDEOS', 'FANS', 'NEWS', 'SHOP'];
 
+    ?>
+
+    <img id="logo" src="/images/dc-logo.png" alt="logo header" />
+    <ul>
+        <li>
+            @foreach ($navlinks as $link)
+                <a href="#">{{ $link }}</a>
+            @endforeach
+        </li>
+    </ul>
 </header>
