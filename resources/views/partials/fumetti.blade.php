e486aa3c42c88bc88023c77c8bfe8274e24d8a0f
giorgios17 <giosur@live.it>
2022-05-19 10:41:00 +0200

    link to page

--- resources/views/partials/fumetti.blade.php
+++ resources/views/partials/fumetti.blade.php
@@ -1,6 +1,8 @@
 @foreach ($fumetti as $fumetto)
     <div class="card">
-        <img src="{{ $fumetto['thumb'] }}" :alt="{{ $fumetto['series'] }}" />
-        <p>{{ $fumetto['series'] }}</p>
+        <a href="/productdetails">
+            <img src="{{ $fumetto['thumb'] }}" :alt="{{ $fumetto['series'] }}" />
+            <p>{{ $fumetto['series'] }}</p>
+        </a>
     </div>
 @endforeach
